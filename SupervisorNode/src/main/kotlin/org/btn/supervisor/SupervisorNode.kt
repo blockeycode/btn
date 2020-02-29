0bac64edde7a74dd27ad5f17a62457857abad138
blockeycode <blockeycode@gmail.com>
2020-02-29 21:19:11 +0800

    Master node bootup.

--- SupervisorNode/src/main/kotlin/org/btn/supervisor/SupervisorNode.kt
+++ SupervisorNode/src/main/kotlin/org/btn/supervisor/SupervisorNode.kt
@@ -1,21 +1,27 @@
 package org.btn.supervisor
 
+import io.netty.bootstrap.ServerBootstrap
+import io.netty.channel.ChannelOption
 import org.btn.common.*
 import org.btn.core.Block
 import org.btn.core.NodeInstance
+import io.netty.channel.nio.NioEventLoopGroup
+import io.netty.channel.socket.nio.NioServerSocketChannel
 
-abstract class SupervisorNode: NodeInstance(){
+
+
+abstract class SupervisorNode : NodeInstance() {
     @ExperimentalStdlibApi
-    fun sealBlock(block: Block){
-        if(!block.verify())
+    fun sealBlock(block: Block) {
+        if (!block.verify())
             return
 
         val signedData = sign(privateKey, block.hashCode)
         val hash = hash(signedData)
         val precedingZeroCount = leadingZeroCount(hash)
-        if(precedingZeroCount <= btnNetwork.sealPrecedingThreshold)
+        if (precedingZeroCount <= btnNetwork.sealPrecedingThreshold)
             return
 
-        block.clerk.seal(signedData,publicKey)
+        block.clerk.seal(signedData, publicKey)
     }
 }
